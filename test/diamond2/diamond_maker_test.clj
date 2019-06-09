bae1b03b7dd23a79ca03898975160587431d0350
Andy Smith <andysmith836@mkodo.com>
2019-06-09 21:29:26 +0100

    Add a few more examples to prove it works

--- test/diamond2/diamond_maker_test.clj
+++ test/diamond2/diamond_maker_test.clj
@@ -29,3 +29,14 @@
   (testing "Get 2 points for A at index 0"
     (is (= 0 (nth (get-points-for-line "A" 0) 0)))
     (is (= 0 (nth (get-points-for-line "A" 0) 1)))))
+
+(deftest points-generator-with-a-b
+  (testing "Get 2 points for B at index 0"
+    (is (= 1 (nth (get-points-for-line "B" 0) 0)))
+    (is (= 1 (nth (get-points-for-line "B" 0) 1))))
+
+  (testing "Get 2 points for B at index 1"
+    (is (= 0 (nth (get-points-for-line "B" 1) 0)))
+    (is (= 2 (nth (get-points-for-line "B" 1) 1))))
+
+  )
