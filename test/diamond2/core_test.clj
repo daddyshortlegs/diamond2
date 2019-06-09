7a2ba0e62f0689715aa35d637d546713fe6e634b
Andy Smith <andysmith836@mkodo.com>
2019-06-09 21:23:35 +0100

    Add code to find char values

--- test/diamond2/core_test.clj
+++ test/diamond2/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [diamond2.core :refer :all]))
 
+(use '[clojure.string :only [index-of]])
+
 
 (defn get-points-for-line [char index]
   [(- 25 index) (+ 25 index)])
@@ -18,7 +20,20 @@
   (testing "Get 2 points for Z at index 25 - Z"
     (is (= 0 (nth (get-points-for-line "Z" 25) 0)))
     (is (= 50 (nth (get-points-for-line "Z" 25) 1))))
-
-
   )
 
+(defn get-index-of-char [char]
+  (index-of "ABCDEFGHIJKLMNOPQRSTUVWXYZ" char))
+
+
+(deftest get-index-for-char-test
+  (testing "index valiues"
+    (is (= 0 (get-index-of-char "A")))
+    (is (= 1 (get-index-of-char "B")))
+    (is (= 25 (get-index-of-char "Z")))))
+
+
+;(deftest points-generator-with-an-a
+;  (testing "Get 2 points for A at index 0"
+;    (is (= 0 (nth (get-points-for-line "A" 0) 0)))
+;    (is (= 0 (nth (get-points-for-line "A" 0) 1)))))
